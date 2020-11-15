94338e81cfb91784f6c7d851cf18c2dd50f8ad6c
Michel Albert <michel@albert.lu>
2020-11-15 11:11:35 +0100

    Simplify handling of default values

--- src/event.js
+++ src/event.js
@@ -2,14 +2,10 @@ import { AppLocale } from "@/applocale.js";
 import { v4 as uuidv4 } from "uuid";
 export class Event {
   constructor(name, startDate, endDate, id) {
-    this.name = name;
-    this.start = startDate;
-    this.end = endDate;
-    if (id) {
-      this.id = id;
-    } else {
-      this.id = uuidv4();
-    }
+    this.name = name || "Unknown";
+    this.start = startDate || null;
+    this.end = endDate || null;
+    this.id = id || uuidv4();
   }
 
   static fromISO(name, startDate, endDate) {
